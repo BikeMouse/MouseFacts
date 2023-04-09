4b95e42e96d790ec7d2992b68863fbe2e02b1478
Judith Kraziewicz <judith.kraziewicz@gmail.com>
2023-04-09 07:54:06 +0100

    added files

---
+++ src/Components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header(){
+    return (
+        <nav className='nav'>
+            <h3>MouseFacts</h3>
+            <h4>Important for Owners</h4>
+        </nav>
+    );
+}
---
+++ src/Components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Main(){
+    return (
+        <div className='Main'>
+            <h1>Fun facts about Mice</h1>
+            <ul>
+                <li>'Mice' is plural for 'Mouse'</li>
+                <li>Mice eat 15 - 20 times a day</li>
+                <li>They can jump, swim and climb easily</li>
+                <li>They are omnivores, which means they eat both plants and meat - usually whatever they can find!</li>
+                <li>A female mouse can give birth to 150 mice in a year!</li>
+                <li>Even if they can live in any conditions, mice usually dig burrows underground to hide from predators.</li>
+                <li>Although they are similar, mice and rats are not the same animals. A rat is usually bigger than a mouse.</li>
+            </ul>
+        </div>
+    );
+}
